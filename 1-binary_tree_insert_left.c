586db2669c776ffa6212060f0fff3f6ad42e9e51
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-10 14:02:07 +0200

    fix: logic of count

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,29 +1,22 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
- * binary_tree_insert_left - Inserts a node as the left-child of a node
- * @parent: Pointer to the parent node
- * @value: Value to store in the new node
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to root node
  *
- * Return: Pointer to the created node, or NULL on failure
+ * Return: Height of tree (edges), 0 if NULL or single node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	binary_tree_t *new_node;
+	size_t left, right;
 
-	if (!parent)
-		return (NULL);
-
-	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = parent->left;
-	new_node->right = NULL;
-	if (parent->left)
-		parent->left->parent = new_node;
-	parent->left = new_node;
-	return (new_node);
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left > right)
+		return (left + 1);
+	return (right + 1);
 }
