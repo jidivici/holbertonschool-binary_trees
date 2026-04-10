bb72b10b3cf57e5fd4b4dbe5dc2b5b54ee2d829b
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-10 14:52:14 +0200

    fix: + test left and right now if is tree with single node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,6 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+    if (!tree->left && !tree->right)
+		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 	if (left > right)
