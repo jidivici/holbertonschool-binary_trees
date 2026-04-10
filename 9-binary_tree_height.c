d25f4a50f4fc73289cd6c7f4ccb960fce6655a31
BriceFTN9 <146724139+BriceFTN9@users.noreply.github.com>
2026-04-10 13:56:31 +0200

    fix: logic of count

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,8 +11,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	left = (binary_tree_height(tree->left) + 1);
-	right = (binary_tree_height(tree->right) + 1);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 	if (left > right)
 		return (left + 1);
 	return (right + 1);
